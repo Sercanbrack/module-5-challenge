c50cbb59e1978caedffe5136fc82bbe7c619bde7
Samuel Ercanbrack <sam.ercanbrack@outlook.com>
2022-05-18 14:00:15 -0600

    Added pseudocode to the script file

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+//Insert current date and time under the header
+
+//Create elements that contain the time, a space to input text, and a save button
+
+//Allow users to input the text into the text block and save it to their local storage using the save button
+
+//Using moment.js, allow the text blocks to dynamically change color based on the time of day
+
